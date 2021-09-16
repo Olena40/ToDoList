7627a9ee9a06a87df37a112eaf03e9552db92ddd
June <norisori@gmail.com>
2021-09-16 11:10:34 +0200

    junes edited datei

--- june.js
+++ june.js
@@ -5,25 +5,22 @@ const myUl = document.querySelector("#myUl");
 const deleteAllBtn = document.querySelector(".footerBtn");
 
 const addTask = () => {
-	myUl.innerHTML += `<li> ${inputField.value}  
-    <button onclick="checkTask(this)"> <i class="fas fa-check-double"></i> </button>
-    <button onclick="editTask(this)"> <i class="fas fa-edit"></i> </button>
-    <button onclick="deleteTask(this)"> <i class="far fa-trash-alt"></i></button>
-     </li>`;
+	myUl.innerHTML += `<li> <p style="display: inline-block"> ${inputField.value} </p> 
+  <button onclick="deleteTask(this)" class="to-do-btn"> <i class="far fa-trash-alt"></i></button>
+  <button onclick="editTask(this)" class="to-do-btn"> <i class="fas fa-edit"></i> </button>
+  <button onclick="checkTask(this)" class="to-do-btn"> <i class="fas fa-check-double"></i> </button>
+   </li>`;
 	inputField.value = "";
 };
 
 const checkTask = (event) => {
-	event.parentElement.toggle();
-};
-
-const editTask = (event) => {
-	event.parentElement.edit();
+	console.log(event.parentElement);
+	event.parentElement.style.textDecoration = "line-through";
+	event.parentElement.style.backgroundColor = "green";
 };
 
 const deleteTask = (event) => {
 	event.parentElement.remove();
 };
-
 
 addBtn.addEventListener("click", addTask);
